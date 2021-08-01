36b1b74c6b3f100c2619b3ce2639c05974b14565
MatiasCeb <matiasceb8812@gmail.com>
2021-07-31 23:46:50 -0300

    [mod] maps API 3

--- project3/src/hooks/useGoogleAddress.js
+++ project3/src/hooks/useGoogleAddress.js
@@ -5,12 +5,11 @@ const useGoogleAddress = address => {
     const [map, setMap] = useState({});
     const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyDH6RIwc1CZVyeJRbD79MSWulfDBYsrGaI`;
 
-    useEffect(async () => {
+    useEffect(async function fetch () {
         {
-                const response = await axios(API)
-                console.log(response)
-                setMap(response.data.results[0].geometry.location);
-        }
+        const response = await axios(API)
+        setMap(response.data.results[0].geometry.location);
+    }
     }, []);
     return map;
 };
