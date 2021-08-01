5c6301f9d5350d98d811c1b7ec56990fa9a4d2e7
MatiasCeb <matiasceb8812@gmail.com>
2021-07-31 23:59:15 -0300

    [mod] Maps API 5

--- project3/src/hooks/useGoogleAddress.js
+++ project3/src/hooks/useGoogleAddress.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 
 const useGoogleAddress = address => {
     const [map, setMap] = useState({});
     const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyDH6RIwc1CZVyeJRbD79MSWulfDBYsrGaI`;
 
-    const loadData = async () => {
+    const loadData = useCallback (async () => {
         const response = await axios(API)
         setMap(response.data.results[0].geometry.location);
-    }
+    }, [API])
 
     useEffect(() => {
-            loadData()
-    }, []);
+        loadData()
+    }, [loadData]);
     return map;
 };
 
